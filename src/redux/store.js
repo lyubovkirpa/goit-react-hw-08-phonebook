2d05b286ce1638c2bacd51e74db427153cc18ca1
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-06-16 01:18:20 +0300

    add redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,39 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-// import { contactReducer } from './contactsSlice';
-// import { filterReducer } from './filterSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { contactReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
-  contacts: [],
-  filter: ""
+  contacts: contactReducer,
+  filter: filterReducer,
 });
 
+const persistConfig = {
+  key: 'contact',
+  storage,
+  blacklist: ['filter'],
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {},
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
