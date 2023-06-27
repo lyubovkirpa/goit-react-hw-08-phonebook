8fcbd0a7cc6f7c5b1ec0b76440e6e087cf80a5e7
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-06-27 23:57:05 +0300

    install library

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 
